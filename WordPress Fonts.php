60a230cbe19fba8f09aeb11817dea2df1599dd4d
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2024-08-13 06:55:47 -0700

    include

--- WordPress Fonts.php
+++ WordPress Fonts.php
@@ -2,3 +2,10 @@
 if ( ! defined( 'ABSPATH' ) ) exit; 
 define ( 'UAF_FILE_PATH', plugin_dir_path( __FILE__ ) );
 
+include UAF_FILE_PATH.'includes/uaf_config.php';
+include UAF_FILE_PATH.'includes/functions/uaf_admin_functions.php';
+include UAF_FILE_PATH.'includes/functions/uaf_client_functions.php';
+include UAF_FILE_PATH.'includes/functions/uaf_font_functions.php';
+include UAF_FILE_PATH.'includes/functions/uaf_cache_functions.php';
+include UAF_FILE_PATH.'includes/functions/uaf_block_editor_functions.php';
+
